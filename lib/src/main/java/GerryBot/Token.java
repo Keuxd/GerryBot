46970e3df342c36028a714db05611098c743f6d6
Keuxd <keuxd2003@gmail.com>
2021-04-27 10:10:45 -0300

    update gitignore

--- lib/src/main/java/GerryBot/Token.java
+++ lib/src/main/java/GerryBot/Token.java
@@ -2,8 +2,8 @@ package GerryBot;
 
 final class Token {
 	
-	private static final String gerryToken = "TOKEN_GERRY";
-	private static final String testertToken = "TOKEN_TESTERT";
+	private static final String gerryToken = "TSDN_GERRY";
+	private static final String testertToken = "TSDN_TESTERT";
 	
 	static String getToken() {
 		if(Main.isTesting) return testertToken;
