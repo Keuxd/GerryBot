2e3a8119153f25a7910023dd6b7223992c4d5794
Keuxd <keuxd2003@gmail.com>
2021-04-26 23:25:48 -0300

    Tst

--- lib/src/main/java/GerryBot/Token.java
+++ lib/src/main/java/GerryBot/Token.java
@@ -5,6 +5,8 @@ final class Token {
 	private static final String gerryToken = "Nzg5OTU5MzQ2MTg3OTI3NTYy.X95o-A.uMbMwry5slQ8dmc3wMNGSQqB62g";
 	private static final String testertToken = "Nzk1OTUwNzg3MTU4OTk5MDYx.X_Q07w.uuBv7-Yj_8Bibcv5E59rK9eyhBQ";
 	
+	
+	
 	static String getToken() {
 		if(Main.isTesting) return testertToken;
 		else return gerryToken;
