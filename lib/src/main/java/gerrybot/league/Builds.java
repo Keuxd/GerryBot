4c0405685701926badf6a24adf845c5ff047d5b6
Keuxd <keuxd2003@gmail.com>
2021-07-19 13:53:05 -0300

    typo + builds clean

--- lib/src/main/java/gerrybot/league/Builds.java
+++ lib/src/main/java/gerrybot/league/Builds.java
@@ -1,7 +1,11 @@
 package gerrybot.league;
 
 import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 import org.jsoup.nodes.Document;
 
@@ -13,11 +17,17 @@ public class Builds {
 		items = new ArrayList<BufferedImage>();
 		downloadBuilds(doc);
 	}
-	
+
 	private void downloadBuilds(Document doc) {
-		doc.getElementsByClass("champion-overview__row champion-overview__row--first").select("img[src~=(?i)\\.(png|jpe?g|gif)]").forEach(element -> {
+		doc.getElementsByClass("champion-stats__list").select("img[src~=(?i)\\.(png|jpe?g|gif)]").forEach(element -> {
 			if(element.attr("src").contains("item")) {
-				
+				try {
+					URLConnection connection = new URL("https:" + element.attr("src").replace("q_auto:", "w_50,h_50&")).openConnection();
+					connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+					items.add(ImageIO.read(connection.getInputStream()));
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
 			}
 		});
 
--- lib/src/main/java/gerrybot/league/League.java
+++ lib/src/main/java/gerrybot/league/League.java
@@ -50,6 +50,6 @@ public class League {
 	public void sendBuilds(MessageChannel channel) throws IOException {
 		File outputFile = new File("cover.png");
 		ImageIO.write(new Draw().drawBuilds(this.builds.getImages()), "png", outputFile);
-		channel.sendFile(outputFile, "covecr.png").queue();
+		channel.sendFile(outputFile, "cover.png").queue();
 	}
 }
