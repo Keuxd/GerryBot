0b725ad5f2f26119866520456b4dd996e142aa77
Keu <keuxd2003@gmail.com>
2021-08-16 16:48:11 -0300

    class created

---
+++ src/gerrybot/hentai/HentaiReader.java
@@ -0,0 +1,6 @@
+package gerrybot.hentai;
+
+//função da classe: 
+public class HentaiReader {
+	//a principio terão 2 arrays[3]; 1: InputFile(paginas do hentai[anterior,atual,proxima]); 2: String(paginas do Hentai url[anterior,atual,proxima])
+}
