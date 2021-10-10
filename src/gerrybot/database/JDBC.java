fe2a73e3b8b9c5a900fb9d30fcdaed5758821311
Keu <keuxd2003@gmail.com>
2021-10-09 21:55:28 -0300

    ResultSet Type/Concur changed
    Since the default ones dont allowed us to move the cursor from row to row, we're now using TYPE_SCROLL_INSENSITIVE

--- src/gerrybot/database/JDBC.java
+++ src/gerrybot/database/JDBC.java
@@ -2,8 +2,11 @@ package gerrybot.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import gerrybot.core.Main;
 
 public class JDBC {
 	public static Connection con;
@@ -11,10 +14,11 @@ public class JDBC {
 	
 	public static void connectDataBase() {
 		try {
-			con = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
-			state = con.createStatement();
+			con = DriverManager.getConnection("jdbc:h2:" + Main.gerryFolder + "/test;MODE=MYSQL", "sa", "");
+			state = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			System.out.println("Database connected");
 		} catch (SQLException e) {
+			e.printStackTrace();
 			System.out.println("Error connecting to Database");
 		}
 	}
