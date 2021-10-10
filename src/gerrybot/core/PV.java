3155b001f1d29caf95f10d704c67827e639ec1bc
Keu <keuxd2003@gmail.com>
2021-10-09 21:49:40 -0300

    added more admin commands in PV
    .downloadbase: since there's a need to sync local database with online database, this will help
    
    .htimers: for hentimers consult
    
    .builds: for items consult
    
    .runes: for runes consult
    
    .sql: if it appears a need to directly input a sql command in database, this will do

--- src/gerrybot/core/PV.java
+++ src/gerrybot/core/PV.java
@@ -1,7 +1,11 @@
 package gerrybot.core;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import gerrybot.database.DataBaseUtils;
+import gerrybot.database.JDBC;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.PrivateChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -15,13 +19,23 @@ public class PV extends ListenerAdapter{
 	public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
 		if(event.getAuthor().isBot()) return;
 		
+		if(!event.getAuthor().getAsTag().equals("Keu#3384")) {
+			event.getChannel().sendMessage("Acesso Negado").queue();
+			return;
+		}
+		
 		String[] args = event.getMessage().getContentRaw().split("\\s+");
 		
 		switch(args[0].toLowerCase()) {
 			case(".help"):{
-				event.getChannel().sendMessage(".set -> Para dar set no user a receber as informacoes.\n"
-											 + ".send '_numbers_' -> Enviar um nHentai aos canais 'henta'.\n"
-											 + ".clear -> Limpar os canais 'henta'.\n"
+				event.getChannel().sendMessage(".set -> Seta o admin user a receber as informacoes.\n"
+											 + ".send '_numbers_' -> Envia um nHentai aos canais 'henta'.\n"
+											 + ".clear -> Limpa os canais 'henta'.\n"
+											 + ".sql '_args_' -> Envia um update command para o banco de dados, use '-' para espaços.\n"
+											 + ".runes -> Retorna os ids e a quantidade de runas unicas no banco de dados.\n"
+											 + ".builds -> Retorna os ids e a quantidade de itens unicos no banco de dados.\n"
+											 + ".htimers -> Retorna os dados da table de htimers.\n"
+											 + ".downloadbase -> Retorna o arquivo central do banco de dados."
 											  ).queue();
 				break;
 			}
@@ -44,7 +58,6 @@ public class PV extends ListenerAdapter{
 				
 				break;
 			}
-			
 			case(".clear"):{
 				try {
 					List<TextChannel> canais = Main.jda.getTextChannelsByName("henta", true);
@@ -59,12 +72,73 @@ public class PV extends ListenerAdapter{
 				}
 				break;
 			}
+			case (".sql"): {
+				try {
+					JDBC.state.executeUpdate(args[1].replace("-", " "));
+					event.getChannel().sendMessage("Comando enviado").queue();
+				} catch(Exception e) {e.printStackTrace();}
+				break;
+			}
+			case(".runes") : {
+				try {
+					ResultSet rs = JDBC.state.executeQuery("select * from runes");
+					if(!rs.next()) {
+						event.getChannel().sendMessage("Runes table is empty").queue();
+						return;
+					}
+					
+					int counter = 0;
+					StringBuilder sb = new StringBuilder();
+					while(rs.next()) {
+						sb.append(rs.getInt("id") + " ");
+						counter++;
+					}
+					event.getChannel().sendMessage(sb + "[" + counter + "]").queue();
+				} catch(Exception e) {}
+				break;
+			}
+			case(".builds") : {
+				try {
+					ResultSet rs = JDBC.state.executeQuery("select * from builds");
+					if(!rs.next()) {
+						event.getChannel().sendMessage("Builds table is empty").queue();
+						return;
+					}
+					
+					int counter = 0;
+					StringBuilder sb = new StringBuilder();
+					while(rs.next()) {
+						sb.append(rs.getInt("id") + " ");
+						counter++;
+					}
+					event.getChannel().sendMessage(sb + "[" + counter + "]").queue();
+					
+				} catch(Exception e) {}
+				break;
+			}
+			case(".downloadbase") : {
+				DataBaseUtils.sendDataBaseFile(event.getChannel());
+				break;
+			}
+			case(".htimers") : {
+				try {
+					ResultSet rs = DataBaseUtils.getHentaTimers();
+					if(!rs.next()) {
+						event.getChannel().sendMessage("DataBase Vazia").queue();
+						return;
+					}
+					rs.beforeFirst();
+					StringBuilder sb = new StringBuilder();
+					while(rs.next()) {
+						sb.append(" [" + rs.getLong("GUILD_ID") + " -> " + rs.getInt("MINUTES") + "] ");
+					}
+					
+					event.getChannel().sendMessage(sb.toString()).queue();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				break;
+			}
 		}
 	}
-	
-	public static void updateHentaState(String lastHenta) {
-		if(myChannel == null) return;
-		myChannel.sendMessage(lastHenta).queue();
-	}
-	
 }
