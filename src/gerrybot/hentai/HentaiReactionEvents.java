9471c8a9e0bfd75ee8679f41ba48b40f665aaa86
Keu <keuxd2003@gmail.com>
2021-10-26 01:36:13 -0300

    ReactionEvents are now in henta package instead of core package
    ReactionEvents class is now in henta package instead of core package and renamed from ReactionEvents to HentaiReactionEvents, 'cause why not :)

--- src/gerrybot/hentai/HentaiReactionEvents.java
+++ src/gerrybot/hentai/HentaiReactionEvents.java
@@ -1,7 +1,77 @@
 package gerrybot.hentai;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import gerrybot.core.Main;
+import gerrybot.database.DataBaseUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed.Field;
+import net.dv8tion.jda.api.entities.MessageReaction.ReactionEmote;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class HentaiReactionEvents extends ListenerAdapter {
+	
+	@Override
+	public void	onMessageReactionAdd(MessageReactionAddEvent event) {
+		ReactionEmote emote = event.getReactionEmote();
+		MessageChannel channel = event.getChannel();
+		long messageId = event.getMessageIdLong();
+		User author = event.getUser();
 
+		if(author.isBot()) return;
+
+		if(channel.getName().equals("henta") && emote.getAsCodepoints().equals("U+1f51e")) {
+			cursedAction(channel, author);
+		}
+		else if(emote.getAsCodepoints().equals("U+2b50")) {
+			try {
+				favoriteAction(channel, author, messageId);
+			} catch (SQLException e) {
+				channel.sendMessage("Error, call the police: " + e.getMessage()).queue();
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void cursedAction(MessageChannel channel, User author) {
+		List<Message> messages = channel.getHistory().retrievePast(30).complete();
+		channel.purgeMessages(messages);
+		
+		channel.sendMessage("O Hentai do dia eh cursed.").queue();
+		channel.sendMessage("https://i.ytimg.com/vi/O8J7yyyFtIU/maxresdefault.jpg").queue();
+		
+		EmbedBuilder embed = new EmbedBuilder();
+		embed.setAuthor(author.getName(), null, author.getAvatarUrl());
+		embed.setColor(Main.cor);
+		
+		channel.sendMessageEmbeds(embed.build()).queue();
+	}
+	
+	private void favoriteAction(MessageChannel channel, User author, long messageId) throws SQLException {
+		Field linkField = null;
+		try { // The message at this point should have 1 embed and 3 fields so we know that's our message, so if any OutOfBoundsException happen in the process, it's not our message.
+			linkField = channel.retrieveMessageById(messageId).complete().getEmbeds().get(0).getFields().get(0);
+		} catch(Exception e) {
+			return;
+		}
+		
+		// If for some reason the condition above get match with the wrong message we'll check the field name and value.
+		String link = linkField.getValue();
+		if(!linkField.getName().equals("Link") && !link.matches("^$"))
+			return;
+		
+		String numbers = link.substring(22, link.length()-1);
+		
+		boolean wasHentaAdded = DataBaseUtils.interactFavoriteHenta(author.getId(), numbers);
+		
+		if(wasHentaAdded) // If it was then we say 'added', if it wasn't then it was 'removed'
+			channel.sendMessage("Henta " + numbers + " added. " + author.getAsMention()).queue();
+		else
+			channel.sendMessage("Henta " + numbers + " removed. " + author.getAsMention()).mention(author).queue();
+	}
 }
