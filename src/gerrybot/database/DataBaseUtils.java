61fea8cd955914103ea905806b5901508ab3e074
Keu <keuxd2003@gmail.com>
2021-10-26 01:38:04 -0300

    added interactFavoriteHenta() and getFavoritesHentas() methods
    since favoriter function and favoritesViewer are now up, the low level communication is here in DataBaseUtils with these new methods

--- src/gerrybot/database/DataBaseUtils.java
+++ src/gerrybot/database/DataBaseUtils.java
@@ -19,11 +19,11 @@ import net.dv8tion.jda.api.entities.PrivateChannel;
 
 public class DataBaseUtils {
 	// Query 'id' n returns the BufferedImage from the base64 string
-	public static BufferedImage getLeagueImage(String id, DataBaseEnum type) {
+	public static BufferedImage getLeagueImage(String id, DataBaseTable type) {
 		try {
 			String sql = String.format("SELECT * FROM %s WHERE ID = %s",
 					type.getTableName(), id);
-			
+
 			ResultSet rs = JDBC.state.executeQuery(sql);
 			rs.next();
 			return convertBase64ToBufferedImage(rs.getString("BASE64"));
@@ -32,7 +32,7 @@ public class DataBaseUtils {
 		}
 	}
 	
-	public static void insertLeagueImage(String id, BufferedImage bi, DataBaseEnum type) {
+	public static void insertLeagueImage(String id, BufferedImage bi, DataBaseTable type) {
 		try {
 			String sql = String.format("INSERT IGNORE INTO %s VALUES(%s,'%s')",
 					type.getTableName(), id, convertBufferedImageToBase64(bi));
@@ -107,15 +107,59 @@ public class DataBaseUtils {
 		int minutes = (Integer.parseInt(time[0]) * 60) + Integer.parseInt(time[1]);
 		
 		String sql = String.format("INSERT INTO %s VALUES(%d,%d) ON DUPLICATE KEY UPDATE MINUTES = %d",
-				DataBaseEnum.GUILDTIMER.getTableName(), guildId, minutes, minutes);
+				DataBaseTable.GUILD_TIMER.getTableName(), guildId, minutes, minutes);
 
 		JDBC.state.executeUpdate(sql);
 	}
 	
 	public static ResultSet getHentaTimers() throws SQLException {
 		String sql = String.format("SELECT * FROM %s",
-				DataBaseEnum.GUILDTIMER.getTableName());
+				DataBaseTable.GUILD_TIMER.getTableName());
 		
 		return JDBC.state.executeQuery(sql);
+	}
+	
+	/**
+	 * It'll check if both values already exist in database, if they do so the respective row will be deleted, if they dont then both values will be inserted.
+	 * @param userId
+	 * @param hentaNumbers
+	 * @throws SQLException If something goes wrong, duh.
+	 * @return Returns true if it did an add action and false if it did a delete action.
+	 */
+	public static boolean interactFavoriteHenta(String userId, String hentaNumbers) throws SQLException {
+		// Check if both values already exist in db
+		// If they do then delete the row
+		// If the dont then insert it
+		String sql = String.format("SELECT CASE WHEN EXISTS(SELECT * FROM %s WHERE %s = %s and %s = %s) THEN true ELSE false END AS RESULT",
+				DataBaseTable.USER_FAVORITE_HENTAS.getTableName(), "USER_ID", userId, "HENTA_CODE", hentaNumbers);
+		
+		ResultSet rs = JDBC.state.executeQuery(sql);
+		rs.first();
+		
+		boolean areBothValuesInDB = rs.getBoolean("RESULT");
+		rs.close();
+		
+		if(areBothValuesInDB)
+			sql = String.format("DELETE FROM %s WHERE %s = %s and %s = %s",
+					DataBaseTable.USER_FAVORITE_HENTAS.getTableName(), "USER_ID", userId, "HENTA_CODE", hentaNumbers);
+		else
+			sql = String.format("INSERT INTO %s VALUES(%s,%s)",
+					DataBaseTable.USER_FAVORITE_HENTAS.getTableName(), userId, hentaNumbers);
+		
+		JDBC.state.executeUpdate(sql);
+		
+		return !areBothValuesInDB;
+	}
+	
+	public static ResultSet getFavoritesHentas(String userId) throws SQLException {
+		String sql = String.format("SELECT %s FROM %s WHERE %s = %s",
+				"HENTA_CODE", DataBaseTable.USER_FAVORITE_HENTAS.getTableName(), "USER_ID", userId);
+		
+		ResultSet rs = JDBC.state.executeQuery(sql);
+		
+		if(!rs.next())
+			throw new SQLException("This user has no favorite hentas in database.");
+		
+		return rs;
 	}
 }
