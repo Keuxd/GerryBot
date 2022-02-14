4960df973a74e6904b2c147fac80ab860d563b05
Keu <76072942+Keuxd@users.noreply.github.com>
2022-02-14 03:24:40 -0300

    getSqlUpdateCommand() created, createTables() "updated"
    createTables() -> Since now DataBaseTable has column names, we don't need them hardcoded, so it was updated, but there are some hardcoded calls yet, it should be changed soon.
    
    getSqlUpdateCommand() -> In order to prevent H2 update problems, this method will return a sql command to insert(transfer) a table.

--- src/gerrybot/database/DataBaseModel.java
+++ src/gerrybot/database/DataBaseModel.java
@@ -5,6 +5,8 @@ import static gerrybot.database.DataBaseTable.ITEM;
 import static gerrybot.database.DataBaseTable.RUNE;
 import static gerrybot.database.DataBaseTable.USER_FAVORITE_HENTAS;
 
+import java.sql.ResultSet;
+
 import gerrybot.league.OpggMetaBase;
 
 public class DataBaseModel {
@@ -14,10 +16,10 @@ public class DataBaseModel {
 	// If the first one gives us an exception then probably the others already exists too, so just return false.
 	public static boolean createTables() {
 		try {
-			JDBC.state.executeUpdate("CREATE TABLE " + ITEM.getTableName() + " (ID INT NOT NULL PRIMARY KEY, ITEM_INFO VARBINARY(256000) NOT NULL)");
-			JDBC.state.executeUpdate("CREATE TABLE " + RUNE.getTableName() + " (ID INT NOT NULL PRIMARY KEY, ITEM_INFO VARBINARY(256000) NOT NULL)");
-			JDBC.state.executeUpdate("CREATE TABLE " + GUILD_TIMER.getTableName() + " (GUILD_ID LONG NOT NULL PRIMARY KEY, MINUTES INT NOT NULL)");
-			JDBC.state.executeUpdate("CREATE TABLE " + USER_FAVORITE_HENTAS.getTableName() + " (USER_ID LONG NOT NULL, HENTA_CODE INT NOT NULL)");
+			JDBC.state.executeUpdate(String.format("CREATE TABLE %s (%s INT NOT NULL PRIMARY KEY, %s VARBINARY(256000) NOT NULL)", ITEM.getTableName(), ITEM.getColumnsNames()[0], ITEM.getColumnsNames()[1]));
+			JDBC.state.executeUpdate(String.format("CREATE TABLE %s (%s INT NOT NULL PRIMARY KEY, %s VARBINARY(256000) NOT NULL)", RUNE.getTableName(), RUNE.getColumnsNames()[0], RUNE.getColumnsNames()[1]));
+			JDBC.state.executeUpdate(String.format("CREATE TABLE %s (%s LONG NOT NULL PRIMARY KEY, %s INT NOT NULL)", GUILD_TIMER.getTableName(), GUILD_TIMER.getColumnsNames()[0], GUILD_TIMER.getColumnsNames()[1]));
+			JDBC.state.executeUpdate(String.format("CREATE TABLE %s (%s LONG NOT NULL, %s INT NOT NULL)", USER_FAVORITE_HENTAS.getTableName(), USER_FAVORITE_HENTAS.getColumnsNames()[0], USER_FAVORITE_HENTAS.getColumnsNames()[1]));
 			
 			System.out.println("All tables were created, since they're all new, builds and runes will be now downloaded.");
 			
@@ -28,6 +30,29 @@ public class DataBaseModel {
 		} catch(Exception e) {
 			System.out.println("Tables already exist.");
 			return false;
+		}
+	}
+	
+	public static String getSqlUpdateCommand(DataBaseTable table) {
+		try {
+			ResultSet rs = JDBC.state.executeQuery("SELECT * FROM " + table.getTableName());
+			
+			StringBuilder sb = new StringBuilder();
+			
+			while(rs.next()) {
+				sb.append('(');
+				sb.append(rs.getString(table.getColumnsNames()[0]));
+				sb.append(',');
+				sb.append(rs.getString(table.getColumnsNames()[1]));
+				sb.append("),");
+			}
+			
+			rs.close();
+			sb.deleteCharAt(sb.length()-1);
+			
+			return String.format("INSERT INTO %s VALUES %s", table.getTableName(), sb.toString());
+		} catch(Exception e) {
+			return "ERROR -> " + e.getMessage();
 		}
 	}
 }
