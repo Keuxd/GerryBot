4cc78f9a464fa1bbcb16c22efd94330188b790d7
Keu <76072942+Keuxd@users.noreply.github.com>
2022-06-08 18:37:32 -0300

    Implemented nhn api access
    Since we had a lot of problems with cloudflare protection, we're now directly accessing nhn using their ip, which gave us access to their api too, so we'll have both ways of scraping, using directly the api, and using it's html.
    
    It'll be kept 'cause the random functionality is implemented as a redirect so it's not an api feature

--- src/gerrybot/hentai/NHentaiNet.java
+++ src/gerrybot/hentai/NHentaiNet.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
@@ -12,39 +13,76 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import gerrybot.league.OpggMetaBase;
+
 public class NHentaiNet {
-	private Document hentaiPageHTML;
+
+	private static final String NHENTAI_IP = "http://35.186.156.165/"; // Singapore
+	private static final String NHENTAI_IP2 = "http://173.82.30.99:3002/"; // California
+	private static final String NHENTAI_COVER = "https://t.nhentai.net/galleries/";
 	
-	public Hentai genHentaiByNumber(String numbers) throws Exception {
-		return genHentai("https://nhentai.net/g/" + numbers);
+	public static Hentai createHentaiByNumber(String numbers) throws Exception {
+		return genHentaiFromJson(NHENTAI_IP2 + "api/gallery/" + numbers);
 	}
 	
-	public Hentai genRandomHentai() throws Exception {
-		return genHentai("https://nhentai.net/random/");
+	public static Hentai createHentaiRandomly() throws Exception {
+		return genHentaiFromHtml(NHENTAI_IP2 + "random");
 	}
 
-	private Hentai genHentai(String initLink) throws Exception {
-		if(!loadLink(initLink))
-			throw new Exception("Invalid hentai");
+	private static Hentai genHentaiFromHtml(String initLink) throws NullPointerException {
+		Document hentaiPageHtml = loadDocument(initLink);
+		if(hentaiPageHtml == null) {
+			throw new NullPointerException("Error loading hentai from HTML");
+		}
+		
 		Hentai nHentaiNet = new Hentai();
 		
-		String link = this.hentaiPageHTML.baseUri();
-		nHentaiNet.setLink(link);
-		nHentaiNet.setNumbers(link.substring(22, link.length()-1));
+		nHentaiNet.setNumbers(hentaiPageHtml.select("h3#gallery_id").text().replace("#", ""));
+		nHentaiNet.setLink("https://nhentai.net/g/" + nHentaiNet.getNumbers());
 		
-		String imageLink = this.hentaiPageHTML.select("img[src~=(?i)\\.(png|jpe?g|gif)]").first().attr("src");
+		String imageLink = hentaiPageHtml.select("img[src~=(?i)\\.(png|jpe?g|gif)]").first().attr("src");
 		nHentaiNet.setCoverLink(imageLink);
 		nHentaiNet.setImageFile(downloadImage(imageLink));
 		
-		nHentaiNet.setTitle(this.hentaiPageHTML.select("h1.title").select("span.pretty").text());
+		nHentaiNet.setTitle(hentaiPageHtml.select("h1.title").select("span.pretty").text());
 		
-		Elements tags = this.hentaiPageHTML.select("div.tag-container.field-name").get(2).select("span.name");
+		Elements tags = hentaiPageHtml.select("div.tag-container.field-name").get(2).select("span.name");
 		nHentaiNet.setTags(genConcatedTags(tags));
 		
 		return nHentaiNet;
 	}
 	
-	private InputStream downloadImage(String imageURL) {
+	private static Hentai genHentaiFromJson(String link) throws NullPointerException {
+		JsonObject jo = OpggMetaBase.downloadJson(link);
+		
+		if(jo == null) {
+			throw new NullPointerException("Error loading hentai from JSON");
+		}
+		
+		Hentai nHentaiNet = new Hentai();
+		nHentaiNet.setNumbers(jo.get("id").getAsString());
+		nHentaiNet.setLink("https://nhentai.net/g/" + nHentaiNet.getNumbers());
+
+		String imageType = jo.get("images").getAsJsonObject().get("cover").getAsJsonObject().get("t").getAsString();
+		
+		String fileName = "/cover." + (imageType.equals("j") ? "jpg" : "png");
+		String imageLink = NHENTAI_COVER + jo.get("media_id").getAsString() + fileName;
+		nHentaiNet.setCoverLink(imageLink);
+		nHentaiNet.setImageFile(downloadImage(imageLink));
+		
+		nHentaiNet.setTitle(jo.get("title").getAsJsonObject().get("pretty").getAsString());
+		
+		JsonArray tags = jo.get("tags").getAsJsonArray();
+		nHentaiNet.setTags(genConcatedTags(tags));
+		
+		return nHentaiNet;
+	}
+	
+	private static InputStream downloadImage(String imageURL) {
 		try {
 			URLConnection connection = new URL(imageURL).openConnection();
 			connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
@@ -55,30 +93,33 @@ public class NHentaiNet {
 		}
 	}
 	
-	// it tries to load the 'Document' for this.hentaiPageHTML, if it can returns true, if it doesn't returns false
-	private boolean loadLink(String link) {
-		// while/if this page doesnt load continue trying loading it
-		while(this.hentaiPageHTML == null) {
+	private static Document loadDocument(String link) {
+		Document hentaiPageHtml = null;
+		
+		// while/if this page doesn't load continue trying loading it
+		while(hentaiPageHtml == null) {
 			try {
-				this.hentaiPageHTML = Jsoup.connect(link).timeout(45*1000).get();
-				return true;
-				
+				hentaiPageHtml =  Jsoup.connect(link)
+						.userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+						.timeout(45*1000)
+						.get();
+				return hentaiPageHtml;
 			} catch(SocketTimeoutException e) {
 				continue;
 			
 			} catch(HttpStatusException e) {
 				e.printStackTrace();
-				return false;
+				return null;
 				
 			} catch(IOException e) {
 				e.printStackTrace();
-				return false;
+				return null;
 			}
 		}
-		return true;
+		return hentaiPageHtml;
 	}
 	
-	protected String genConcatedTags(Elements tags) {
+	private static String genConcatedTags(Elements tags) {
 		if(tags.size() == 0) return "";
 		
 		String hentaiTags = new String();
@@ -88,5 +129,42 @@ public class NHentaiNet {
 		}
 		
 		return hentaiTags.substring(0, hentaiTags.length()-2) + ".";
+	}
+	
+	private static String genConcatedTags(JsonArray array) {
+		if(array.size() == 0) return "";
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(JsonElement element : array) {
+			JsonObject object = element.getAsJsonObject();
+			
+			if(!object.get("type").getAsString().equals("tag")) {
+				continue;
+			}
+			
+			String tag = object.get("name").getAsString();
+			sb.append(tag.substring(0,1).toUpperCase() + tag.substring(1) + ", ");
+		}
+		sb.setCharAt(sb.length() - 2, '.');
+		
+		return sb.toString();
+	}
+	
+	// DEBUG ONLY
+	public static void pingCheck(int IP) {
+		try {
+			Process process = Runtime.getRuntime().exec("ping " + ((IP == 0) ? "35.186.156.165" : "173.82.30.99"));
+			
+			InputStream is = process.getInputStream();
+			Scanner sc = new Scanner(is, "UTF-8");
+			
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+			sc.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
